897745ba09ed6003576dbb7a8610863963ed9913
rajvardhan-ulape <rajvardhanulape2151@gmail.com>
2025-08-20 11:41:03 +0530

    Dypcet add in navbar

--- clgweb/src/components/Navbar.js
+++ clgweb/src/components/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">MyCampus</div>
+      <div className="logo">DYPCET</div>
       <ul className="nav-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About Us</a></li>
